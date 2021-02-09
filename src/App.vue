d51d8adbae853165c2b85af08a2cbb670bb6c491
Alexandr <sasha-is-permi@mail.ru>
2021-02-09 17:00:28 +0500

    '31'

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,134 @@
-
-
-  <!-- Директива app - свойство, которое говорит vuetify
-  о том как правильно следить за поведением элементов 
-  drawer- всплывающее меню слева 
-  toolbar- верхняя часть приложения
-  content- основная часть приложения
-  footer- подвал
-  container - контейнер как в bootstrap.
-  Данные строки превращаются в обычные div с классами. 
-  
-  -->
-
-
 <template>
-<!-- App.vue -->
+  <v-container>
+    <v-row justify="space-around">
+      <v-col cols="12">
+        <v-select
+          v-model="color"
+          :items="colors"
+          label="Color"
+        ></v-select>
+      </v-col>
 
-<v-app>
-  <!-- не работает
-  <v-navigation-drawer  app>
+      <v-switch
+        v-model="drawer"
+        class="ma-2"
+        label="v-model"
+      ></v-switch>
 
-  </v-navigation-drawer> -->
+      <v-switch
+        v-model="permanent"
+        class="ma-2"
+        label="Permanent"
+      ></v-switch>
 
-  <v-app-bar app>
-    <!-- -->  app-bar
-  </v-app-bar>
+      <v-switch
+        v-model="miniVariant"
+        class="ma-2"
+        label="Mini variant"
+      ></v-switch>
 
-  <!-- Sizes your content based upon application components -->
-  <v-main>
+      <v-switch
+        v-model="expandOnHover"
+        class="ma-2"
+        label="Expand on hover"
+      ></v-switch>
 
-    <!-- Provides the application the proper gutter -->
-    <v-container fluid>
-      container
-      <!-- If using vue-router -->
-      <router-view></router-view>
-    </v-container>
-  </v-main>
+      <v-switch
+        v-model="background"
+        class="ma-2"
+        label="Background"
+      ></v-switch>
 
-  <v-footer app>
-    <!-- --> footer
-  </v-footer>
-</v-app>
+      <v-switch
+        v-model="right"
+        class="ma-2"
+        label="Right"
+      ></v-switch>
+    </v-row>
+
+    <v-card
+      height="400"
+      class="overflow-hidden"
+    >
+      <v-navigation-drawer
+        v-model="drawer"
+        :color="color"
+        :expand-on-hover="expandOnHover"
+        :mini-variant="miniVariant"
+        :right="right"
+        :permanent="permanent"
+        :src="bg"
+        absolute
+        dark
+      >
+        <v-list
+          dense
+          nav
+          class="py-0"
+        >
+          <v-list-item two-line :class="miniVariant && 'px-0'">
+            <v-list-item-avatar>
+              <img src="https://randomuser.me/api/portraits/men/81.jpg">
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>Application</v-list-item-title>
+              <v-list-item-subtitle>Subtext</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+  </v-container>
 </template>
+
 
 <script>
   export default {
-
+    data () {
+      return {
+        drawer: true,
+        items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+        color: 'primary',
+        colors: [
+          'primary',
+          'blue',
+          'success',
+          'red',
+          'teal',
+        ],
+        right: false,
+        permanent: true,
+        miniVariant: false,
+        expandOnHover: false,
+        background: false,
+      }
+    },
+    computed: {
+      bg () {
+        return this.background ? 'https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg' : undefined
+      },
+    },
   }
 </script>
+ 
