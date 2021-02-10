5c50bf7dfed8f69f046543cbf84f489284d0024f
Alexandr <sasha-is-permi@mail.ru>
2021-02-10 10:24:24 +0500

    '3'

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,36 @@
 <template>
   <v-container>
-    <v-row justify="space-around">
+    <v-row >
       <v-col cols="12">
-        <v-select
-          v-model="color"
-          :items="colors"
-          label="Color"
-        ></v-select>
+             <h1 class="h1"> Административная панель </h1>     
       </v-col>
-
-      <v-switch
-        v-model="drawer"
-        class="ma-2"
-        label="v-model"
-      ></v-switch>
-
-      <v-switch
-        v-model="permanent"
-        class="ma-2"
-        label="Permanent"
-      ></v-switch>
-
-      <v-switch
-        v-model="miniVariant"
-        class="ma-2"
-        label="Mini variant"
-      ></v-switch>
-
-      <v-switch
-        v-model="expandOnHover"
-        class="ma-2"
-        label="Expand on hover"
-      ></v-switch>
-
-      <v-switch
-        v-model="background"
-        class="ma-2"
-        label="Background"
-      ></v-switch>
-
-      <v-switch
-        v-model="right"
-        class="ma-2"
-        label="Right"
-      ></v-switch>
     </v-row>
-
+      
     <v-card
-      height="400"
-      class="overflow-hidden"
+      style="margin-top:20px;" 
+      height="600"
+      width="250"
     >
       <v-navigation-drawer
         v-model="drawer"
         :color="color"
-        :expand-on-hover="expandOnHover"
+        :expand-on-hover="xpandOnHover"
         :mini-variant="miniVariant"
         :right="right"
         :permanent="permanent"
         :src="bg"
         absolute
-        dark
       >
         <v-list
           dense
           nav
           class="py-0"
         >
-          <v-list-item two-line :class="miniVariant && 'px-0'">
-            <v-list-item-avatar>
-              <img src="https://randomuser.me/api/portraits/men/81.jpg">
-            </v-list-item-avatar>
+          <v-list-item >
 
             <v-list-item-content>
-              <v-list-item-title>Application</v-list-item-title>
-              <v-list-item-subtitle>Subtext</v-list-item-subtitle>
+              <v-list-item-title>User</v-list-item-title>
+              <v-list-item-subtitle>Иванов А.В.</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
 
@@ -89,7 +46,7 @@
             </v-list-item-icon>
 
             <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
+              <v-list-item-title class="list-item-title">{{ item.title }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list>
@@ -105,30 +62,42 @@
       return {
         drawer: true,
         items: [
-          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-          { title: 'Photos', icon: 'mdi-image' },
-          { title: 'About', icon: 'mdi-help-box' },
+          { title: 'Рабочие пространства', icon: 'mdi-view-dashboard' },
+          { title: 'Разработчики', icon: 'mdi-view-dashboard' },
+          { title: 'Команды', icon: 'mdi-view-dashboard' },
+          { title: 'Проекты', icon: 'mdi-view-dashboard' },
+          { title: 'Группы проектов', icon: 'mdi-view-dashboard' },        
+          { title: 'Процессы', icon: 'mdi-view-dashboard' },
+          { title: 'Метрики', icon: 'mdi-view-dashboard' },
+          { title: 'Интеграции', icon: 'mdi-view-dashboard' },
+          { title: 'Настройки', icon: 'mdi-view-dashboard' },
+          { title: 'Интеграции', icon: 'mdi-view-dashboard' }
         ],
-        color: 'primary',
-        colors: [
-          'primary',
-          'blue',
-          'success',
-          'red',
-          'teal',
-        ],
+        color: 'blue',
         right: false,
         permanent: true,
         miniVariant: false,
-        expandOnHover: false,
-        background: false,
+        expandOnHover: false
       }
     },
     computed: {
       bg () {
-        return this.background ? 'https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg' : undefined
-      },
-    },
+ //       return 'https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg'
+       return ""
+      }
+    }
   }
 </script>
+
+<style scoped>
+   .h1{
+       text-align:center;
+       color:blue;       
+   }
+   .list-item-title {
+      font-size:18px!important;
+      padding-left:10px;
+   }
+
+</style>
  
