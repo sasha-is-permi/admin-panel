b4e68e1df2dacf60092453d941a713452026f286
Alexandr <sasha-is-permi@mail.ru>
2021-03-10 12:04:32 +0500

    '39'

--- src/store/employee.js
+++ src/store/employee.js
@@ -155,149 +155,389 @@ employees
 
 */
 
-export default {
+/*
+
+class employees {
+    constructor (id, lastName, firstName, middleName,gender,
+birthDate,email,login,portals,projects,team) {
+      this.id = id
+      this.lastName = lastName
+      this.firstName = firstName
+      this.middleName = middleName
+      this.gender = gender
+      this.birthDate = birthDate
+      this.email = email
+      this.login = login
+      this.portals = portals
+      this.projects = projects
+      this.team = team      
+    }            
+    }
+  }
+
+  */
+  
+  export default {
     state: {  
-       // Пробные данные для проверки работы программы 
-        employees: [           
-                    {id:"1",
-                     lastName:"Иванов",
-                     firstName:"Артем",
-                     middleName:"Аркадьевич",
-                     gender:"м",
-                     birthDate:"15.07.75",              
-                     email:"ivanov@mail.ru",
-                     login:"ivanov",
-                     "portals":[
-                      {id:"1",
-                       name:"http://jira/ivanov",
-                       integrationType:{
-                        id:"1",
-                        name:"jira1.0",
-                        "code":"jira"   
+        // Пробные данные для проверки работы программы 
+         employees: [           
+                     {id:"1",
+                      lastName:"Иванов",
+                      firstName:"Артем",
+                      middleName:"Аркадьевич",
+                      gender:"м",
+                      birthDate:"15.07.75",              
+                      email:"ivanov@mail.ru",
+                      login:"ivanov",
+                      portals:[
+                        {id:"1",
+                        integrationType:{
+                         id:"1",
+                         name:"jira",
+                         code:"jira"                  
+                        },
+                        portalId:"http://jira1"
+                        },
+                        {id:"2",
+                        integrationType:{
+                         id:"2",
+                         name:"Jira ServiceDesk",
+                         code:"JiraServiceDesk"                  
+                        },
+                        portalId:"http://JiraServiceDesk1"
+                        }                 
+                            ],
+                     projects:[
+                        {id:"1",
+                         name:"Проект 1",
+                         group: {id:"1",name:"Группа проектов 1",            
+                         workspace: {id:"1",name:"workspace1",active: false,
+                         logo: "https://img2.fonwall.ru/o/wr/roza-rozy-purpurnye-rozy-dsij.jpg"}
+                         },
+                         processes:[], 
+                         portals:[]
+                        },
+                    
+                    
+                        {id:"2",
+                        name:"Проект 2",
+                        group: {id:"2",name:"Группа проектов 2",            
+                        workspace: {id:"2",name:"workspace2",active: true,
+                        logo: "https://img2.fonwall.ru/o/wr/roza-rozy-purpurnye-rozy-dsij.jpg"}
+                        },
+                        processes: [], 
+                        portals:[]
                        },
-                       portalId:"jira25" },
-                           {id:"2",
-                       name:"http://git/ivanov",
-                       integrationType:{
-                        id:"2",
-                        name:"git1.0",
-                        code:"git"   
+                    
+                    
+                       {id:"3",
+                       name:"Проект 3",
+                       group: {id:"3",name:"Группа проектов 3",            
+                       workspace: {id:"3",name:"workspace3",active: true,
+                       logo: "https://img2.fonwall.ru/o/wr/roza-rozy-purpurnye-rozy-dsij.jpg"}
                        },
-                       portalId:"git2" }
+                       processes:
+                       [ ], 
+                       portals:[]
+                      }
+                        
+                          ],
+                     team:   {id:"1",name:"Команда № 1",
+                        bitrixPortalId:"http://bitrix/hardworker",
+                        projects: [
+                         {name:"Проект 1",
+                          group:{},
+                          processes:[],
+                          portals:[]
+                         },
+                         {name:"Проект 3",
+                         group:{},
+                         processes:[],
+                         portals:[]
+                         }  
+                         ]            
+                        }
+         },          
+           {id:"2",
+            lastName:"Петров",
+            firstName:"Денис",
+            middleName:"Артемьевич",
+            gender:"м",
+            birthDate:"28.05.56",              
+            email:"petrov@mail.ru",
+            login:"petrov",
+            portals:[
+                {id:"2",
+                integrationType:{
+                 id:"2",
+                 name:"jira ServiceDesk",
+                 code:"jiraServiceDesk "                  
+                },
+                portalId:"http://jiraServiceDesk2"
+                },
+                {id:"3",
+                integrationType:{
+                 id:"3",
+                 name:"Git",
+                 code:"Git"                  
+                },
+                portalId:"http://git3"
+                }                 
                     ],
-                    projects: [
-                        {id:"25",name:"project1"},
-                        {id:"26",name:"project2"}
-                                ],
-                    team: {id:"1",name:"Команда № 1"}
-        },          
-          {id:"2",
-           lastName:"Петров",
-           firstName:"Денис",
-           middleName:"Артемьевич",
-           gender:"м",
-           birthDate:"28.05.56",              
-           email:"petrov@mail.ru",
-           login:"petrov",
-           "portals":[
-            {id:"1",
-             name:"http://jira/petrov",
-             integrationType:{
-              id:"1",
-              name:"jira1.0",
-              "code":"jira"   
-             },
-             portalId:"jira25" },
-                 {id:"2",
-             name:"http://git/petrov",
-             integrationType:{
-              id:"2",
-              name:"git1.0",
-              code:"git"   
-             },
-             portalId:"git2" }
-          ],
-          projects: [
-              {id:"25",name:"project1"},
-              {id:"26",name:"project2"}
-                      ],
-          team: {id:"2",name:"Команда № 2"}
-          },                 
-            {id:"1",
-             lastName:"Савельева",
-             firstName:"Елена",
-             middleName:"Геннадьевна",
-             gender:"ж",
-             birthDate:"15.04.85",              
-             email:"saveljeva@mail.ru",
-             login:"saveljeva",
-             "portals":[
-              {id:"1",
-               name:"http://jira/saveljeva",
-               integrationType:{
-                id:"1",
-                name:"jira1.0",
-                "code":"jira"   
-               },
-               portalId:"jira25" },
-                   {id:"2",
-               name:"http://git/saveljeva",
-               integrationType:{
-                id:"2",
-                name:"git1.0",
-                code:"git"   
-               },
-               portalId:"git2" }
-            ],
-            projects: [
-                {id:"25",name:"project1"},
-                {id:"26",name:"project4"}
-                        ],
-            team: {id:"2",name:"Команда № 2"}
-             }
-            ]   
+             projects:[
+                        {id:"1",
+                         name:"Проект 1",
+                         group: {id:"1",name:"Группа проектов 1",            
+                         workspace: {id:"1",name:"workspace1",active: false,
+                         logo: "https://img2.fonwall.ru/o/wr/roza-rozy-purpurnye-rozy-dsij.jpg"}
+                         },
+                         processes:[], 
+                         portals:[]
+                        },
+                    
+                    
+                        {id:"2",
+                        name:"Проект 2",
+                        group: {id:"2",name:"Группа проектов 2",            
+                        workspace: {id:"2",name:"workspace2",active: true,
+                        logo: "https://img2.fonwall.ru/o/wr/roza-rozy-purpurnye-rozy-dsij.jpg"}
+                        },
+                        processes: [], 
+                        portals:[]
+                       },
+                    
+                    
+                       {id:"3",
+                       name:"Проект 3",
+                       group: {id:"3",name:"Группа проектов 3",            
+                       workspace: {id:"3",name:"workspace3",active: true,
+                       logo: "https://img2.fonwall.ru/o/wr/roza-rozy-purpurnye-rozy-dsij.jpg"}
+                       },
+                       processes:
+                       [ ], 
+                       portals:[]
+                      }
+                        
+                          ], 
+           team:  {id:"2",name:"Команда № 2",
+           bitrixPortalId:"http://bitrix/soworker",
+           projects: [
+            {name:"Проект 2",
+             group:{},
+             processes:[],
+             portals:[]
+            },
+            {name:"Проект 3",
+            group:{},
+            processes:[],
+            portals:[]
+            }  
+            ]      
+            }
+           },                 
+             {id:"1",
+              lastName:"Савельева",
+              firstName:"Елена",
+              middleName:"Геннадьевна",
+              gender:"ж",
+              birthDate:"15.04.85",              
+              email:"saveljeva@mail.ru",
+              login:"saveljeva",
+              portals:[
+                {id:"3",
+                integrationType:{
+                 id:"3",
+                 name:"Git",
+                 code:"Git"                  
+                },
+                portalId:"http://git3"
+                },
+                {id:"4",
+                integrationType:{
+                 id:"4",
+                 name:"Gitlab",
+                 code:"Gitlab"                  
+                },
+                portalId:"http://gitlab3"
+                }                 
+                    ],
+                projects:[
+                        {id:"1",
+                         name:"Проект 1",
+                         group: {id:"1",name:"Группа проектов 1",            
+                         workspace: {id:"1",name:"workspace1",active: false,
+                         logo: "https://img2.fonwall.ru/o/wr/roza-rozy-purpurnye-rozy-dsij.jpg"}
+                         },
+                         processes:[], 
+                         portals:[]
+                        },
+                    
+                    
+                        {id:"2",
+                        name:"Проект 2",
+                        group: {id:"2",name:"Группа проектов 2",            
+                        workspace: {id:"2",name:"workspace2",active: true,
+                        logo: "https://img2.fonwall.ru/o/wr/roza-rozy-purpurnye-rozy-dsij.jpg"}
+                        },
+                        processes: [], 
+                        portals:[]
+                       },
+                    
+                    
+                       {id:"3",
+                       name:"Проект 3",
+                       group: {id:"3",name:"Группа проектов 3",            
+                       workspace: {id:"3",name:"workspace3",active: true,
+                       logo: "https://img2.fonwall.ru/o/wr/roza-rozy-purpurnye-rozy-dsij.jpg"}
+                       },
+                       processes:
+                       [ ], 
+                       portals:[]
+                      }
+                        
+                          ], 
+             team: {id:"3",name:"Команда № 3",
+             bitrixPortalId:"http://bitrix/command3",
+             projects: [
+              {name:"Проект 1",
+               group:{},
+               processes:[],
+               portals:[]
+              },
+              {name:"Проект 2",
+              group:{},
+              processes:[],
+              portals:[]
+              }  
+              ]         
+             }    
+             
+              }
+             ]   
+ 
+            // ads: [],
+            // id:"" 
+     }, 
+          mutations: { 
+            // удаление элемента
+            deleteEmployee (state, {id}) {                          
+              // находим - какой индекс у удаляемого элемента в массиве  из state
+              const index = state.employees.findIndex(a => a.id === id);
+              
+              console.log("delete element index",index);
+              // в массиве из state employees начиная с позиции 1 удалить 1 элемент
+              state.employees.splice(index, 1);
+              },
 
-           // ads: [],
-           // id:"" 
-    },
-    mutations: { 
-     //   employees(state, payload) {
-    //        state.employees = payload
-     //    }
-
-       },
-    actions: {  
-      //  setLoading ({commit}, payload) {
-            // Вызываем commit с mutation setLoading
-      //      commit('employees', payload)
-      //    },
-        //  employees ({commit}, payload) {
-          //  commit('employees', payload)
-        //  },
+              // добавление элемента
+              addEmployee (state, payload) {         
          
-        // Удаление сотрудника из базы
-          deleteItem(){
-              alert('Сотрудник удален');
-          },
-        // Добавление нового сотрудника в базу     
-          addItem(){
-            alert('Элемент добавлен');
-        },
-        // Редактирование существующего сотрудника 
-        // в базе
-        editItem(){
-            alert('Элемент отредактирован');
-        }
+              //let a = {};
+             // console.log('payload1',payload)
+             // console.log( payload.id,payload.name,)           
+             // a.id=payload["employees"]["id"];
+            //  a.name = payload["employees"]["name"];             
+            
+        //   console.log("add",payload.id,payload.name,a) 
+        console.log("id,lastName,firstName,middleName,gender,birthDate,email,login,portals,projects,team", 
+        payload.id, payload.lastName, payload.firstName, payload.middleName,
+        payload.gender,payload.birthDate,payload.email,payload.login,
+        payload.portals,payload.projects,payload.team)
 
-        },       
+
+state.employees.push({id:payload.id, lastName:payload.lastName,
+    firstName:payload.firstName, middleName:payload.middleName,
+    gender:payload.gender,birthDate:payload.birthDate,
+    email:payload.email,login:payload.login,
+    portals:payload.portals,projects:payload.projects,
+    team:payload.team
+})
+
+
+                // console.log(state.employees)
+
+                // this.renderComponent = true
+            
+                },
+              // редактирование элемента
+              editEmployee (state, payload) {         
+                 // обновляем элемент в массиве state.employees
+              // find находит элементы по условию. Берет первый попавшийся.
+              // Поскольку id уникален- можно так.
+              const element = state.employees.find(a => {
+                return a.id === payload.id
+            })
+             // Присваиваем найденному элементу
+             // новые значения, полученные из формы
+             element["lastName"] = payload["lastName"]
+             element["firstName"] = payload["firstName"]
+             element["middleName"] = payload["middleName"]
+             element["gender"] = payload["gender"]
+             element["birthDate"] = payload["birthDate"]
+             element["email"] = payload["email"]
+             element["login"] = payload["login"]
+             element["portals"] = payload["portals"]
+             element["projects"] = payload["projects"]
+             element["team"] = payload["team"]
+
+             // let a = {};
+             //console.log('payload1',payload)
+            // console.log( payload.id,payload.name)
+
+             // a.id=payload["employees"]["id"];
+             // a.name = payload["employees"]["name"];       
+             
+             // element["employees"] = a
+
+           // element["employees"] = payload["employees"]
+
+            console.log("edit", element["lastName"],
+            element["firstName"], element["middleName"], 
+            element["gender"], element["birthDate"],
+            element["email"], element["login"],
+            element["portals"], element["projects"],
+            element["team"] )  }  
+              },
+           actions: {  
+               
+            // Удаление процесса из массива в state
+            // payload- полученный удаляемый элемент (деструктурируем до id)
+                 deleteEmployee({commit},{id}){
+                   // вызываем mutation deleteId
+                   // Передаем ему полученный из формы id элемента для удаления
+                    commit('deleteEmployee',{id})
+                 },
+
+
+
+            // добавление процесса в массив из state
+            addEmployee({commit},payload){
+              console.log('payload',payload)
+              // Формируем случайным образом id для нового элемента
+              const id = Math.round(Math.random()*1000000000).toString();
+              payload.id = id;
+              // вызываем mutation addEmployee 
+              // Передаем ему полученные из формы данные для добавления
+               commit('addEmployee',payload)
+            },
+
+            editEmployee({commit},payload){
+              // вызываем mutation editEmployee 
+              // Передаем ему полученные из формы данные для редактирования
+                commit('editEmployee',payload)
+            },
+               },      
         
     getters: {
-         employees(state) {
-             // Возвращаем всех сотрудников по запросу 
-             // из файла vue
-             return state.employees
-         }   
+        employees(state) {
+            // Возвращаем всех сотрудников по запросу 
+            // из файла vue
+            return state.employees
+        }   
+        
          }
 
 
 }
+
+
